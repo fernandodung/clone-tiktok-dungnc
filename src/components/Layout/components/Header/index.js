c9bfe0ceb568578f18678e22c6051296689f22a5
dung2000 <dungnc@rikkeisoft.com>
2022-11-11 17:20:41 +0700

    update

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,29 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+import images from "../../../../assets/images";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            heder
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx('logo')}>
+          <img src= {images.logo} alt= 'tiktok'/>
         </div>
+        <div className={cx('Search')}>
+          <input placeholder="Search accounts and videos" spellCheck = {false}/>
+          <button className={cx('clear')}>
+            clear
+          </button>
+          {/*loading*/}
+          <button className={cx('search-btn')}>
+            {/*search*/ }
+          </button>
+        </div>
+      </div>
     </header>
+  );
 }
 
 export default Header;
